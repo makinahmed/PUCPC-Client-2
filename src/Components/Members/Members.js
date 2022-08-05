8338f0c1af219d4f07e89f262b57472a710fffff
Makin Ahmed <ahmedmakin555@gmail.com>
2022-08-05 17:17:57 +0600

    added final

--- src/Components/Members/Members.js
+++ src/Components/Members/Members.js
@@ -14,23 +14,24 @@ export default function Members() {
    
   return (
     <div className="my-5 mx-auto w-75">
-      <h2 className="text-center my-5">CLUB MEMBERS</h2>
+      <h2 className="text-center my-5">List Of Official Members</h2>
 
       <table class="table table-striped">
         <thead>
           <tr>
             <th scope="col text-center">Name</th>
             <th scope="col  text-center">Email</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Batch</th>
+            <th scope="col">ID</th>
           </tr>
         </thead>
         <tbody>
           {members?.map((member) => (
             <tr>
-              
               <td>{member.name}</td>
               <td>{member.email}</td>
-              <td>Batch</td>
+              <td>{member?.id}</td>
+              <td>{member?.batch}</td>
             </tr>
           ))}
         </tbody>
